8aaec7cb97865236c2f04fe5a8628ad553fb1bfe
RafaelRossau <rafaelrossau@gmail.com>
2025-11-14 11:59:44 -0300

    dados-omori

--- public/script.js
+++ public/script.js
@@ -44,80 +44,80 @@ function carregaPersonagem(){
   let sorte_extra = document.getElementById("sorte_extra");
   let chance_hit_extra = document.getElementById("chance_hit_extra");
 
-//     fetch(url4)
-//     .then((response) => {
-//       return response.json();
-//     })
-
-//     .then((Armamento) => {
-      
-//   console.log(Armamento)
-//   let FK_nome_armamento= "Armamento: " + Armamento[id].nome
-//   nome_armamento.innerHTML = FK_nome_armamento
-  
-// })
-  
-//   fetch(url3)
-//     .then((response) => {
-//       return response.json();
-//     })
-
-//     .then((Comidas) => {
-      
-//   console.log(Comidas)
-//   let FK_nome_comidas = "Comida: " + Comidas[id].nome
-//   nome_comidas.innerHTML = FK_nome_comidas
-  
-// })
-  
-//   fetch(url2)
-//     .then((response) => {
-//       return response.json();
-//     })
-
-//     .then((Habilidades) => {
-      
-//   console.log(Habilidades)
-//   let FK_nome_habilidades = "Habilidade: " + Habilidades[id].nome
-//   nome_habilidades.innerHTML = FK_nome_habilidades
-  
-// })
-fetch(url5)
+    fetch(url4)
     .then((response) => {
       return response.json();
-     })
+    })
 
-    .then((PersonagensTESTE) => {
+    .then((Armamento) => {
       
-  console.log(PersonagensTESTE)
-  nomeTESTE.innerHTML = PersonagensTESTE[id].nome
-  aniversarioTESTE.innerHTML = PersonagensTESTE[id].nascimento
-  ataqueTESTE.innerHTML = PersonagensTESTE[id].ataque
-  defesaTESTE.innerHTML =PersonagensTESTE[id].defesa
- })
+  console.log(Armamento)
+  let FK_nome_armamento= "Armamento: " + Armamento[id].nome
+  FK_nome_armamento = nome_armamento
   
+})
   
-// fetch(url)
+  fetch(url3)
+    .then((response) => {
+      return response.json();
+    })
+
+    .then((Comidas) => {
+      
+  console.log(Comidas)
+  let FK_nome_comidas = "Comida: " + Comidas[id].nome
+  nome_comidas.innerHTML = FK_nome_comidas
+  
+})
+  
+  fetch(url2)
+    .then((response) => {
+      return response.json();
+    })
+
+    .then((Habilidades) => {
+      
+  console.log(Habilidades)
+  let FK_nome_habilidades = "Habilidade: " + Habilidades[id].nome
+  nome_habilidades.innerHTML = FK_nome_habilidades
+  
+})
+// fetch(url5)
 //     .then((response) => {
 //       return response.json();
-//     })
+//      })
 
-//     .then((Personagens) => {
+//     .then((PersonagensTESTE) => {
+      
+//   console.log(PersonagensTESTE)
+//   nomeTESTE.innerHTML = PersonagensTESTE[id].nome
+//   aniversarioTESTE.innerHTML = PersonagensTESTE[id].nascimento
+//   ataqueTESTE.innerHTML = PersonagensTESTE[id].ataque
+//   defesaTESTE.innerHTML =PersonagensTESTE[id].defesa
+//  })
+  
+  
+fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+
+    .then((Personagens) => {
       
 
-//   console.log(Personagens)
-//   nome_personagem.innerHTML = "Nome: " + Personagens[id].nome
-//   vida.innerHTML = "Vida: " + Personagens[id].vida
-//   suco.innerHTML = "Suco: " + Personagens[id].suco
-//   ataque.innerHTML = "Ataque:" + Personagens[id].ataque
-//   defesa.innerHTML = "Defesa: " + Personagens[id].defesa
-//   velocidade.innerHTML = "Velocidade: " + Personagens[id].velocidade
-//   sorte.innerHTML = "Sorte: " + Personagens[id].sorte
-//   aniversario.innerHTML ="Aniversário: " + Personagens[id].aniversario
-      
+  console.log(Personagens)
+  nome_personagem.innerHTML = "Nome: " + Personagens[id].nome
+  vida.innerHTML = "Vida: " + Personagens[id].vida
+  suco.innerHTML = "Suco: " + Personagens[id].suco
+  ataque.innerHTML = "Ataque:" + Personagens[id].ataque
+  defesa.innerHTML = "Defesa: " + Personagens[id].defesa
+  velocidade.innerHTML = "Velocidade: " + Personagens[id].velocidade
+  sorte.innerHTML = "Sorte: " + Personagens[id].sorte
+  aniversario.innerHTML ="Aniversário: " + Personagens[id].aniversario
+  
   
     
-//})
+})
 
     .catch((error) => {
       console.log("erro no carregamento do personagem");
@@ -125,32 +125,33 @@ fetch(url5)
 }
 
  function criarPersonagem(submit){
-//   const nomeadicao = document.getElementById("nomeadicao").value
-//   const vidaadicao = document.getElementById("vidaadicao").value
-//   const sucoadicao = document.getElementById("sucoadicao").value
-//   const ataqueadicao = document.getElementById("ataqueadicao").value
-//   const defesaadicao = document.getElementById("defesaadicao").value
-//   const velocidadeadicao = document.getElementById("velocidadeadicao").value
-//   const sorteadicao = document.getElementById("sorteadicao").value
-//   const aniversarioadicao = document.getElementById("aniversarioadicao").value
-//   const habilidadesadicao = document.getElementById("habilidadesadicao").value
-//   const comidasadicao = document.getElementById("comidasadicao").value
-//   const armamentoadicao = document.getElementById("armamentoadicao").value
+  
+  const nomeadicao = document.getElementById("nomeadicao").value
+  const vidaadicao = document.getElementById("vidaadicao").value
+  const sucoadicao = document.getElementById("sucoadicao").value
+  const ataqueadicao = document.getElementById("ataqueadicao").value
+  const defesaadicao = document.getElementById("defesaadicao").value
+  const velocidadeadicao = document.getElementById("velocidadeadicao").value
+  const sorteadicao = document.getElementById("sorteadicao").value
+  const aniversarioadicao = document.getElementById("aniversarioadicao").value
+  const habilidadesadicao = document.getElementById("habilidadesadicao").value
+  const comidasadicao = document.getElementById("comidasadicao").value
+  const armamentoadicao = document.getElementById("armamentoadicao").value
 
 
-//    fetch("/Personagens", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({nomeadicao, vidaadicao, sucoadicao, ataqueadicao, defesaadicao, velocidadeadicao, sorteadicao, aniversarioadicao, habilidadesadicao, comidasadicao, armamentoadicao }), // manda o objeto js para o banco como json
-//   });
-const nomeAdicaoTeste = document.getElementById("nomeadicaoTESTE").value
-const nascimentoAdicaoTeste = document.getElementById("nascimentoadicaoTESTE").value
-const ataqueAdicaoTeste = document.getElementById("ataqueadicaoTESTE").value
-const defesaAdicaoTeste = document.getElementById("defesaadicaoTESTE").value
+   fetch("/Personagens", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({nomeadicao, vidaadicao, sucoadicao, ataqueadicao, defesaadicao, velocidadeadicao, sorteadicao, aniversarioadicao, habilidadesadicao, comidasadicao, armamentoadicao }), // manda o objeto js para o banco como json
+  });
+// const nomeAdicaoTeste = document.getElementById("nomeadicaoTESTE").value
+// const nascimentoAdicaoTeste = document.getElementById("nascimentoadicaoTESTE").value
+// const ataqueAdicaoTeste = document.getElementById("ataqueadicaoTESTE").value
+// const defesaAdicaoTeste = document.getElementById("defesaadicaoTESTE").value
 
-fetch("/PersonagensTESTE", {
-     method: "POST",
-     headers: { "Content-Type": "application/json" },
-     body: JSON.stringify({nomeAdicaoTeste, nascimentoAdicaoTeste, ataqueAdicaoTeste, defesaAdicaoTeste}), // manda o objeto js para o banco como json
-   });
+// fetch("/PersonagensTESTE", {
+//      method: "POST",
+//      headers: { "Content-Type": "application/json" },
+//      body: JSON.stringify({nomeAdicaoTeste, nascimentoAdicaoTeste, ataqueAdicaoTeste, defesaAdicaoTeste}), // manda o objeto js para o banco como json
+//    });
  };
